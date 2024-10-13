e665a369e5225fa35f897956f87677c767bf5557
Faya <osvaldocarvalho.profissional@gmail.com>
2024-10-13 17:19:49 -0300

    Adicionados argumentos argc e argv na função main para receber os programas por argumento

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "lex.yy.h"
 
-int main() {
+int main(int argc, char ** argv)
+{
+
     char * yytext;
-    return initLexer();
+
+    if (argc > 0)
+        return initLexer(argv[1]);
 
     /*todo: resolver os problemas com os comentários
      *todo: tabela de simbolos
@@ -11,3 +15,5 @@ int main() {
      * */
 
 }
+
+/*oeeeeeeeeee*/
